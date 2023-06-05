ca5d1ac10889f1db4def4d248849c9b7271ffb4f
faresbo <fareselbouali@gmail.com>
2023-06-05 15:28:08 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+/*
+setInterval(function () {
+
+	if( (parseInt($("#info").css("width")) / 300) < 2 ){
+			 $("#info").css("width", "340px" );
+	}else 
+	
+	if( (parseInt($("#info").css("width")) / 300) < 3 &&  (parseInt($("#info").css("width")) / 300) >= 2 ){
+			 $("#info").css("width", "640px" );
+	}else
+	 {
+			 $("#info").css("width", "900px" );
+	}
+	
+
+
+}, 100);
+
+*/
